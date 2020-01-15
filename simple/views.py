eb063a17dd9edd8798852d71c9d31a4564e5da1a
jon <you@example.com>
2020-01-15 13:30:42 +0300

    done with the sign up page's front-end

--- simple/views.py
+++ simple/views.py
@@ -4,4 +4,13 @@ from flask import render_template,redirect,url_for,request
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+#create_an_account
+@app.route('/register')
+def create_account():
+    return render_template('singup.html')
+
+#sign_in
+@app.route('/login')
+def sign_in_user():
+    return render_template('login.html')
